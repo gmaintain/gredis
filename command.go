0adef3e2b74bad40efa5d888efafaacf82a61613
yifanes <yifanes@qq.com>
2019-11-13 18:40:01 +0800

    完成雏形,构建请求

--- command.go
+++ command.go
@@ -1,18 +1,9 @@
 package gredis
 
-import (
-	"bufio"
-	"log"
-	"net"
-)
-
-func Get(conn *net.TCPConn, str string) (string, error) {
-
-	reader := bufio.NewReader(conn)
-	_, _ = conn.Write([]byte(str + "\r\n"))
-
-	//strResp,_ := reader.ReadString('\n')
-	strResp, _ := reader.ReadBytes('\n')
-	log.Printf("read string resp: %#v", string(strResp))
-	return string(strResp), nil
+func (c *RedisConn) Get(key string) ([]byte, error) {
+	_, err := c.Call("GET", key)
+	if err != nil {
+		return nil, err
+	}
+	return nil, err
 }
--- conn.go
+++ conn.go
@@ -3,8 +3,10 @@ package gredis
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"log"
 	"net"
+	"strconv"
 	"time"
 )
 
@@ -43,6 +45,46 @@ func Dial(address string, timeout time.Duration) (*net.TCPConn, error) {
 	return conn, nil
 }
 
+func (c *RedisConn) Call(command string, args ...interface{}) (interface{}, error) {
+	var err error
+	if err = c.request(command, args); err != nil {
+		return nil, err
+	}
+	if err = c.BuffioWriter.Flush(); err != nil {
+		return nil, err
+	}
+	return nil, err
+}
+
+func (c *RedisConn) request(command string, args []interface{}) error {
+	var err error
+	if err = c.writeLen('*', 1+len(args)); err != nil {
+		return err
+	}
+	if err = c.writeString(command); err != nil {
+		return err
+	}
+	for _, arg := range args {
+		switch data := arg.(type) {
+		case string:
+			err = c.writeString(data)
+		case int64:
+			err = c.writeInt64(data)
+		case int:
+			err = c.writeInt64(int64(data))
+		case bool:
+			if data == true {
+				err = c.writeString("1")
+			} else {
+				err = c.writeString("0")
+			}
+		default:
+			err = c.writeString(fmt.Sprintf("%v", data))
+		}
+	}
+	return err
+}
+
 // 协议格式,写入命令或者参数的长度
 func (c *RedisConn) writeLen(prefix byte, n int) error {
 	pos := len(c.Buffer) - 1
@@ -58,4 +100,34 @@ func (c *RedisConn) writeLen(prefix byte, n int) error {
 	c.Buffer[pos] = prefix
 	_, err := c.BuffioWriter.Write(c.Buffer[pos:])
 	return err
+}
+
+//协议第二步,写入命令长度以及命令
+func (c *RedisConn) writeString(command string) error {
+	var err error
+	if err = c.writeLen('$', len(command)); err != nil {
+		return err
+	}
+	if _, err = c.BuffioWriter.WriteString(command + "\r\n"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *RedisConn) writeInt64(n int64) error {
+	return c.writeBytes(strconv.AppendInt([]byte{}, n, 10))
+}
+
+func (c *RedisConn) writeBytes(b []byte) error {
+	var err error
+	if err = c.writeLen('$', len(b)); err != nil {
+		return err
+	}
+	if _, err = c.BuffioWriter.Write(b); err != nil {
+		return err
+	}
+	if _, err = c.BuffioWriter.WriteString("\r\n"); err != nil {
+		return err
+	}
+	return nil
 }
